4e086d8664dfa3b39514d093a84b6b822f6501e2
Andrey Esipenko <mralcapone96@gmail.com>
2020-02-03 12:32:08 +0200

    tabs, timer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 window.addEventListener('DOMContentLoaded', function() {
 
     'use strict';
+
+    // Tabs
+
     let tab = document.querySelectorAll('.info-header-tab'),
         info = document.querySelector('.info-header'),
         tabContent = document.querySelectorAll('.info-tabcontent');
@@ -32,4 +35,53 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     })
+
+    //Timer
+
+    let deadline = '2020-02-04';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+
+            if (seconds < 10) {
+                seconds = '0' + Math.floor((t/1000) % 60)
+            } if ( minutes < 10) {
+                minutes = "0" + Math.floor((t/1000/60) % 60)
+            } if (hours < 10) {
+                hours = "0" + Math.floor((t/(1000*60*60)));
+            }
+
+        return {
+                'total' : t,
+                'hours' : hours,
+                'minutes' : minutes,
+                'seconds' : seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    };
+    setClock('timer', deadline);
 });
